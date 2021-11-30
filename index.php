f67049c34824d5f391f63a2fcb3e44d614abdfc1
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2021-11-30 13:28:38 -0500

    Update index.php
    Updating to use Carbon.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-printf("Right now is %s", Carbon::now()->toDateTimeString());
+require __DIR__ . '/vendor/autoload.php';
+use Carbon\Carbon;
 
-printf("Right now in New York is %s", Carbon::now('US/Eastern'));
+
+printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
+
+printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
+
+printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
